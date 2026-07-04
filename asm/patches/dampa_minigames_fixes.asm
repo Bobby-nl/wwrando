b14dc1a0019df4e6cb44b965ba8139271b22549c
Bobby-nl <265284095+Bobby-nl@users.noreply.github.com>
2026-07-04 14:06:40 +0200

    Fix Dampa Pig Minigame - Fix locking yourself out of finishing the minigame if you take damage while the pigs are near Dampa
    Ported over from HD rando: https://github.com/SuperDude88/TWWHD-Randomizer/commit/f28d28918165e9c30dcd9f6759679d79ccc89d2e

--- asm/patches/dampa_minigames_fixes.asm
+++ asm/patches/dampa_minigames_fixes.asm
@@ -1,6 +1,11 @@
-; This patch changes both of Dampa's Pig minigames so they can only be completed once, 
-; thus allowing safe randomization without duplicating certain progression items.
-; Removes unnecessary restrictions for minigame 1.
+; This patch changes both of Dampa's Pig minigames so they can only be completed once,
+; allowing them to be safely randomized without duplicating certain progression items.
+; It also removes unnecessary restrictions from Minigame 1.
+;
+; Fixes a bug where, in either of Dampa's Pig minigames, the player can lock themselves
+; out of completing the game by taking damage at the same moment a pig is "seen" by Dampa.
+; In that situation, the pig is marked as counted, but Dampa's dialogue (and several
+; related flags required for progression) are not updated correctly.
 
 .open "files/rels/d_a_npc_people.rel"
 
@@ -29,4 +34,26 @@
 .org 0x8AB0
   bne 0x8AD8
 
+; If Dampa already has a pending pig-count dialogue (mEtcFlag & 0x80),
+; do not allow any additional pigs to be counted.
+; Instead, reorder Dampa's speak event so the pending dialogue can be
+; displayed, then return immediately.
+; Once the dialogue is shown, the game clears mEtcFlag bit 0x80 and
+; pig-counting can resume normally.
+
+.org 0x9544 ; in daNpcPeople_c::checkPig
+  b dampa_check_for_pending_pigs
+.org @NextFreeSpace
+.global dampa_check_for_pending_pigs
+dampa_check_for_pending_pigs:
+  ; Pointer to Dampa's actor is in r30
+  lwz r11, 0x758(r30) ; Load mEtcFlag
+  andi. r11, r11, 0x80 ; mEtcFlag & 0x80
+  bne try_reorder_speak_event ; A pig is currently waiting for text to be shown
+  lbz r0, 0x789(r30) ; Replace the line we overwrote to jump here
+  b 0x9548 ; Continue to see if this pig meets the requirements to be counted
+try_reorder_speak_event:
+  mr r3, r30
+  bl fopAcM_orderSpeakEvent__FP10fopAc_ac_c
+  b 0x96a0 ; Return without counting any new pigs
 .close
